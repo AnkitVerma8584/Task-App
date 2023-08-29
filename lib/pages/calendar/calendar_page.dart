3a99775c21cf48d2ac74057fefa8aa4e608fcdf7
AnkitVerma8584 <av8584060@gmail.com>
2023-08-29 15:03:27 +0530

    Date picker added

--- lib/pages/calendar/calendar_page.dart
+++ lib/pages/calendar/calendar_page.dart
@@ -1,40 +1,112 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:month_year_picker/month_year_picker.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:task_app/pages/calendar/date_picker/date_picker.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
 
   @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime selected = DateTime.now();
+  CalendarController controller = CalendarController();
+
+  @override
   Widget build(BuildContext context) {
-    final today = DateTime.now();
+    Appointment appointment = Appointment(
+        startTime: selected,
+        endTime: selected.copyWith(hour: 20),
+        color: Colors.blue,
+        subject: "Open website",
+        notes: "Do this",
+        resourceIds: ["", ""]);
+
+    Appointment appointment1 = Appointment(
+        startTime: selected,
+        endTime: selected.copyWith(day: 30),
+        color: Colors.green,
+        subject: "Open wrbsite");
+    Appointment appointment2 = Appointment(
+        startTime: selected,
+        endTime: selected.copyWith(hour: 18),
+        color: Colors.purple,
+        subject: "Do something");
     return SafeArea(
         child: Scaffold(
-      body: Column(children: [
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         TextButton(
-            onPressed: () async {
-              final selected = await showMonthYearPicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2019),
-                  lastDate: DateTime(2030));
+            onPressed: () {
+              showMonthYearPicker(
+                      context: context,
+                      initialDate: selected,
+                      firstDate: DateTime(2019),
+                      lastDate: DateTime(2030))
+                  .then((value) {
+                if (value != null) {
+                  setState(() {
+                    selected = value;
+                  });
+                }
+              });
             },
-            child: const Text("Year month")),
-        DatePicker(selectedMonth: today),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    DateFormat('MMMM yyyy').format(selected),
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                  const Icon(Icons.arrow_drop_down)
+                ],
+              ),
+            )),
+        MyDatePicker(
+            selectedMonth: selected,
+            onDateSelected: (dt) {
+              setState(() {
+                selected = selected.copyWith(day: dt.day);
+                controller.selectedDate = selected;
+                controller.displayDate = selected;
+              });
+            }),
+        Expanded(
+            child: SfCalendar(
+          controller: controller,
+          dataSource: TaskDataSource([appointment, appointment1, appointment2]),
+          appointmentBuilder: myAppointmentBuilder,
+          viewNavigationMode: ViewNavigationMode.none,
+        ))
       ]),
     ));
   }
 }
 
-List<DateTime> getDateTimeList(DateTime selectedMonth) {
-  int totalDays = daysInMonth(selectedMonth);
-  return List.generate(totalDays,
-      (index) => DateTime(selectedMonth.year, selectedMonth.month, index));
+Widget myAppointmentBuilder(BuildContext context,
+    CalendarAppointmentDetails calendarAppointmentDetails) {
+  final Appointment appointment = calendarAppointmentDetails.appointments.first;
+  return Container(
+    height: calendarAppointmentDetails.bounds.height,
+    width: calendarAppointmentDetails.bounds.width,
+    color: appointment.color.withOpacity(0.2),
+    padding: const EdgeInsets.all(5),
+    alignment: Alignment.center,
+    child: Text(
+      appointment.subject,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+    ),
+  );
 }
 
-int daysInMonth(DateTime date) {
-  DateTime firstDayThisMonth = DateTime(date.year, date.month, 1);
-  var firstDayNextMonth = DateTime(firstDayThisMonth.year,
-      firstDayThisMonth.month + 1, firstDayThisMonth.day);
-  return firstDayNextMonth.difference(firstDayThisMonth).inDays;
+class TaskDataSource extends CalendarDataSource {
+  TaskDataSource(List<Appointment> source) {
+    appointments = source;
+  }
 }
--- lib/pages/calendar/date_picker/date_picker.dart
+++ lib/pages/calendar/date_picker/date_picker.dart
@@ -3,33 +3,97 @@ import 'package:intl/intl.dart';
 import 'package:task_app/common/components/sized_boxes.dart';
 import 'package:task_app/theme/colors.dart';
 
-class DatePicker extends StatefulWidget {
-  const DatePicker({super.key, required this.selectedMonth});
+class MyDatePicker extends StatelessWidget {
+  const MyDatePicker(
+      {super.key, required this.selectedMonth, required this.onDateSelected});
   final DateTime selectedMonth;
-
-  @override
-  State<DatePicker> createState() => _DatePickerState();
-}
-
-class _DatePickerState extends State<DatePicker> {
-  late List<DateTime> timeList;
-  @override
-  void initState() {
-    super.initState();
-    timeList = getDateTimeList(widget.selectedMonth);
-  }
+  final void Function(DateTime) onDateSelected;
 
   @override
   Widget build(BuildContext context) {
+    final List<DateTime> timeList = getDateTimeList(selectedMonth);
     return SizedBox(
       height: 90,
       child: timeList.isNotEmpty
           ? ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: timeList.length,
-              itemBuilder: (context, index) => DateTimeItemCard(
-                  date: timeList[index], isSelected: index == 2))
+              itemBuilder: (context, index) => InkWell(
+                  borderRadius: BorderRadius.circular(8),
+                  onTap: () => onDateSelected(timeList[index]),
+                  child: DateTimeItemCard(
+                      date: timeList[index],
+                      isSelected: selectedMonth.day == timeList[index].day)))
           : null,
+    );
+  }
+}
+
+// class DatePicker extends StatefulWidget {
+//   const DatePicker(
+//       {super.key, required this.selectedMonth, required this.onDateSelected});
+//   final DateTime selectedMonth;
+//   final void Function(DateTime) onDateSelected;
+
+//   @override
+//   State<DatePicker> createState() => _DatePickerState();
+// }
+
+// class _DatePickerState extends State<DatePicker> {
+//   late List<DateTime> timeList;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     timeList = getDateTimeList(widget.selectedMonth);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 90,
+//       child: timeList.isNotEmpty
+//           ? ListView.builder(
+//               scrollDirection: Axis.horizontal,
+//               itemCount: timeList.length,
+//               itemBuilder: (context, index) => DateTimeItemCard(
+//                   date: timeList[index], isSelected: index == 0))
+//           : null,
+//     );
+//   }
+// }
+
+class DateTimeItemCard extends StatelessWidget {
+  const DateTimeItemCard(
+      {super.key, required this.date, required this.isSelected});
+  final DateTime date;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Text(DateFormat("E").format(date).toUpperCase()),
+          sh8,
+          Container(
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: isSelected
+                      ? getColorsScheme(context).primary
+                      : Colors.grey.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text(
+                date.day.toString(),
+                style: TextStyle(
+                    color: isSelected
+                        ? getColorsScheme(context).onPrimary
+                        : getColorsScheme(context).primary),
+              ))
+        ],
+      ),
     );
   }
 }
@@ -45,43 +109,4 @@ int daysInMonth(DateTime date) {
   var firstDayNextMonth = DateTime(firstDayThisMonth.year,
       firstDayThisMonth.month + 1, firstDayThisMonth.day);
   return firstDayNextMonth.difference(firstDayThisMonth).inDays;
-}
-
-class DateTimeItemCard extends StatelessWidget {
-  const DateTimeItemCard(
-      {super.key, required this.date, required this.isSelected});
-  final DateTime date;
-  final bool isSelected;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(8),
-      onTap: () {},
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(DateFormat("E").format(date).toUpperCase()),
-            sh8,
-            Container(
-                height: 40,
-                width: 40,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: isSelected
-                        ? getColorsScheme(context).primary
-                        : Colors.grey.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(30)),
-                child: Text(
-                  date.day.toString(),
-                  style: TextStyle(
-                      color: isSelected
-                          ? getColorsScheme(context).onPrimary
-                          : getColorsScheme(context).primary),
-                ))
-          ],
-        ),
-      ),
-    );
-  }
 }
